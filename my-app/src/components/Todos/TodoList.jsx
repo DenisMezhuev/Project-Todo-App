575447e3b2ebd1ba2948d7e3fc3bb3ec7ef458e0
Denis Ermakov <alexmezhuev@yandex.by>
2023-06-03 22:30:26 +0300

    feat: add button delete

--- my-app/src/components/Todos/TodoList.jsx
+++ my-app/src/components/Todos/TodoList.jsx
@@ -1,14 +1,19 @@
 import Todo from "./Todo";
 import styles from "./TodoList.module.css";
 
-function TodoList({ todos, deleteTodo }) {
+function TodoList({ todos, deleteTodo, toggleTodo }) {
   return (
     <div className={styles.todoListContainer}>
       {!todos.length ? (
         <h3>Todo list is empty</h3>
       ) : (
-        todos.map((todo, index) => (
-          <Todo key={index} todo={todo} index={index} deleteTodo={deleteTodo} />
+        todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+          />
         ))
       )}
     </div>
---
+++ my-app/src/components/Todos/TodosActions.jsx
@@ -0,0 +1,12 @@
+function TodosActions() {
+  return (
+    <>
+      <button>Reset</button>
+      <button>Delete Completed</button>
+    </>
+  );
+}
+
+export default TodosActions;
+
+
